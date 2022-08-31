f629124784839c9beb5afafbcb6451673192e43c
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-08-31 01:36:00 -0300

    Add routes/index and routes/auth. Add auth controller

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import routes from './routes/index.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true, limit: '50mb'}))
 app.use(cors())
+app.use('/', routes)
 
 export default app
---
+++ api/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const login = async(req, res) => {
+  res.status(200).send('Login')
+}
+
+export const register = async(req, res) => {
+  res.status(200).send('Register')
+}
