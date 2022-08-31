2c827d6723f9686b010b92845b1865084069af35
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-08-31 01:46:16 -0300

    Add characters routes & controllers

---
+++ api/src/controllers/characters.controller.js
@@ -0,0 +1,25 @@
+export const getCharacters = async(req, res) => {
+  res.status(200).send('Get characters')
+}
+
+export const getCharacter = async(req, res) => {
+  const { id } = req.params
+
+  res.status(200).send(`Get ${id} detail`)
+}
+
+export const createCharacter = async(req, res) => {
+  res.status(200).send('Create character')
+}
+
+export const updateCharacter = async(req, res) => {
+  const { id } = req.params
+
+  res.status(200).send(`Update ${id} character`)
+}
+
+export const deleteCharacter = async(req, res) => {
+  const { id } = req.params
+
+  res.status(200).send(`Delete ${id} character`)
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import authRoutes from './auth.js'
+import charactersRoutes from './characters.js'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
+router.use('/characters', charactersRoutes)
 
 export default router
