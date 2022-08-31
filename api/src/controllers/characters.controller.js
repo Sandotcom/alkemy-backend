f9de541083a3381947d4615e88fbedda06aec3a6
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-08-31 03:02:41 -0300

    Add getCharacters, getCharacter & createCharacter controllers

--- api/src/controllers/characters.controller.js
+++ api/src/controllers/characters.controller.js
@@ -1,18 +1,40 @@
 import Characters from '../models/Characters.js'
 
 export const getCharacters = async(req, res) => {
+  try {
+    const characters = await Characters.findAll()
 
-  res.status(200).send('Get characters')
+    res.status(200).send(characters)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' })
+  }
 }
 
 export const getCharacter = async(req, res) => {
   const { id } = req.params
 
-  res.status(200).send(`Get ${id} detail`)
+  try {
+    const character = await Characters.findByPk(id)
+
+    if(!character) return res.status(404).json({ message: 'Character not found.' })
+
+    res.status(200).send(character)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' })
+    
+  }
 }
 
 export const createCharacter = async(req, res) => {
-  res.status(200).send('Create character')
+  const { image, name, age, weight, history } = req.body
+
+  try {
+    const character = await Characters.create({ image, name, age, weight, history })
+
+    res.status(200).send(character)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' })    
+  }
 }
 
 export const updateCharacter = async(req, res) => {
